97962d13d53134ba1d230e7d7fa08dcbca09ae6d
sagittariusolli <56923094+sagittariusolli@users.noreply.github.com>
2022-03-29 11:45:40 +0500

    Revert "Frontend"

--- NodeManager.Web/Models/FamSymbolViewModel.cs
+++ NodeManager.Web/Models/FamSymbolViewModel.cs
@@ -24,7 +24,5 @@ namespace NodeManager.Web.Models
             if (str.Length < 27) return str;
             else return str.Substring(0, 27) + "...";
         }
-
-        public string userName = null;
     }
 }
--- NodeManager.Web/Models/RegisterModel.cs
+++ NodeManager.Web/Models/RegisterModel.cs
@@ -4,9 +4,6 @@ namespace NodeManager.Web.Models
 {
     public class RegisterModel
     {
-        [Required(ErrorMessage = "Не указано имя")]
-        public string Name { get; set; }
-
         [Required(ErrorMessage = "Не указан Email")]
         public string Email { get; set; }
 
