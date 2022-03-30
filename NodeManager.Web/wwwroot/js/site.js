467f9966800f9794957169472d671961d2ce9070
Anna <anyatropina@mail.ru>
2022-03-30 21:34:18 +0500

    Unfinished search by tag

--- NodeManager.Web/wwwroot/js/site.js
+++ NodeManager.Web/wwwroot/js/site.js
@@ -2,3 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//$(".chosen-select").chosen({
+//    disable_search_threshold: 10,
+//    no_results_text: "Oops, nothing found!",
+//    width: "95%"
+//});
+
+//$('#city').chosen().change(function () {
+
+//    var id = $(this).val();
+//    console.log(id); //индекс выбранного элемента
+
+//});
+
+//$("#city").trigger("chosen:updated"); //обновляем select     
+
+////$.validator.setDefaults({ ignore: ":hidden:not(select)" }) //для всех тегов select
+
+//$.validator.setDefaults({ ignore: ":hidden:not(.chosen-select)" }) //только для select имеющий class .chosen-select
+
+/*$(selector).selected({ [options] });*/
+
+$(document).ready(function () {
+    $('.chosen-select').chosen({ width: "200px" });
+});
+
+
+
+//$('select').chosen({ width: "200px" });
+
+//$('select').chosen({ disable_search_threshold: 10 });
+
+//$('select').chosen({ no_results_text: "Not found" });
+
+//$('select').chosen({ max_selected_options: 2 });
+
+//$('select').chosen({ allow_single_deselect: true });
+
