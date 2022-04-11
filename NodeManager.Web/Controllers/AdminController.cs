37a03c035f1a68cb45e9ac99feaf1722e7d5846d
DimaSaGit <56923094+sagittariusolli@users.noreply.github.com>
2022-04-11 11:02:59 +0500

    added controller and model for admin panel

---
+++ NodeManager.Web/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using NodeManager.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using NodeManager.Web.Abstract;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using NodeManager.Web.Models;
+using System.Drawing;
+using System.IO;
+using NodeManager.Web.Repository;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace NodeManager.Web.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private INodes repos;
+
+        public IActionResult GetAllUsers()
+        {
+            var model = new AllUsersModel()
+            {
+                Users = repos.Users.ToList(),
+                UserName = HttpContext.User.Identity.Name,
+                IsLogin = HttpContext.User.Identity.IsAuthenticated,
+            };
+            return View(model);
+        }
+
+    }
+}
---
+++ NodeManager.Web/Models/AllUsersModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using NodeManager.Domain;
+using NodeManager.Web.Abstract;
+
+namespace NodeManager.Web.Models
+{
+    public class AllUsersModel : IUser
+    {
+        public string UserName { get; set; }
+        public bool IsLogin { get; set; }
+        public List<Users> Users { get; set; }
+
+    }
+}
