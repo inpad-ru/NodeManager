0276c3f98fa92eb1965039ddb7a046aa0420a982
Anna <anyatropina@mail.ru>
2022-06-01 23:21:03 +0500

    pagination

--- NodeManager.Web/wwwroot/js/site.js
+++ NodeManager.Web/wwwroot/js/site.js
@@ -2,27 +2,8 @@
     $('.chosen-select').chosen({ width: "250px" });
 });
 
-var inputs = document.querySelectorAll('.upload-file__input');
-Array.prototype.forEach.call(inputs, function (input) {
-    var label = input.nextElementSibling,
-        labelVal = label.innerHTML;
+function redirectPage(numPage) {
+    var urlPage = window.location.href;
 
-    input.addEventListener('change', function (e) {
-        var fileName = '';
-        if (this.files && this.files.length > 1)
-            fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-        else
-            fileName = e.target.value.split('\\').pop();
-
-        if (fileName.length > 18)
-            fileNameCut = fileName.substr(0, 11) + '…' + fileName.substr(name.length - 6);
-        else fileNameCut = fileName;
-
-        if (fileNameCut)
-            label.querySelector('span').innerHTML = fileNameCut;
-        else
-            label.innerHTML = labelVal;
-        
-        document.getElementsByClassName("upload-file__text")[0].title = fileName;
-    });
-});
+    console.log(urlPage);
+}
