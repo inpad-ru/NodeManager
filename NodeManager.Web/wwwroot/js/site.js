cf56ed769e8d22d2a430b18b76ba2374424e3750
A1604 <anyatropina@mail.ru>
2022-06-02 16:42:48 +0500

    pagination wtf

--- NodeManager.Web/wwwroot/js/site.js
+++ NodeManager.Web/wwwroot/js/site.js
@@ -5,14 +5,29 @@
 function redirectPage(numPage) {
     var urlPage = window.location.href;
     var decodedURLPage = decodeURI(urlPage);
+    //console.log(urlPage);
+    //console.log(decodedURLPage);
     var arrURLPage = decodedURLPage.split('/');
+    //console.log(arrURLPage.length);
+    //console.log(arrURLPage);
+    //console.log(decodedURLPage);
+    if (arrURLPage.length == 4)
+    {
+        var newArrURLPage = new Array(6);
+        newArrURLPage[0] = arrURLPage[0];
+        newArrURLPage[1] = arrURLPage[1];
+        newArrURLPage[2] = arrURLPage[2];
+        newArrURLPage[3] = "Node";
+        newArrURLPage[4] = "List";
+        newArrURLPage[5] = numPage;
+        var newMinURLPage = newArrURLPage.join('/');
+        /*return encodeURI(newMinURLPage);*/
+        //console.log(newArrURLPage);
+        //console.log(encodeURI(newMinURLPage));
+    }
     arrURLPage[5] = numPage;
     var newURLPage = arrURLPage.join('/');
     var encodedURLPage = encodeURI(newURLPage);
-    return encodedURLPage;
-    console.log(newURLPage);
+    console.log(encodedURLPage);
     //return encodedURLPage;
-    //console.log(decodedURLPage);
-    //console.log(arrURLPage);
-    //console.log(encodedURLPage);
 }
