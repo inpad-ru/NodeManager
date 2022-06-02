b02291e13e172c649cf9f0aa30066ad506656bca
A1604 <anyatropina@mail.ru>
2022-06-02 13:15:50 +0500

    pagination 2

--- NodeManager.Web/wwwroot/js/site.js
+++ NodeManager.Web/wwwroot/js/site.js
@@ -4,6 +4,15 @@
 
 function redirectPage(numPage) {
     var urlPage = window.location.href;
-
-    console.log(urlPage);
+    var decodedURLPage = decodeURI(urlPage);
+    var arrURLPage = decodedURLPage.split('/');
+    arrURLPage[5] = numPage;
+    var newURLPage = arrURLPage.join('/');
+    var encodedURLPage = encodeURI(newURLPage);
+    return encodedURLPage;
+    console.log(newURLPage);
+    //return encodedURLPage;
+    //console.log(decodedURLPage);
+    //console.log(arrURLPage);
+    //console.log(encodedURLPage);
 }
