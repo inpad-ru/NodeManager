7a46f8a6114a71c6f9e277776fe5705137a7f890
A1604 <anyatropina@mail.ru>
2022-06-10 16:01:19 +0500

    pagination temp

--- NodeManager.Web/wwwroot/js/site.js
+++ NodeManager.Web/wwwroot/js/site.js
@@ -2,39 +2,9 @@
     $('.chosen-select').chosen({ width: "250px" });
 });
 
-//function redirectPage(numPage) {
-//    var urlPage = window.location.href;
-//    var decodedURLPage = decodeURI(urlPage);
-//    //console.log(urlPage);
-//    //console.log(decodedURLPage);
-//    var arrURLPage = decodedURLPage.split('/');
-//    //console.log(arrURLPage.length);
-//    //console.log(arrURLPage);
-//    //console.log(decodedURLPage);
-//    if (arrURLPage.length == 4)
-//    {
-//        var newArrURLPage = new Array(6);
-//        newArrURLPage[0] = arrURLPage[0];
-//        newArrURLPage[1] = arrURLPage[1];
-//        newArrURLPage[2] = arrURLPage[2];
-//        newArrURLPage[3] = "Node";
-//        newArrURLPage[4] = "List";
-//        newArrURLPage[5] = numPage;
-//        var newMinURLPage = newArrURLPage.join('/');
-//        /*return encodeURI(newMinURLPage);*/
-//        //console.log(newArrURLPage);
-//        //console.log(encodeURI(newMinURLPage));
-//    }
-//    else if (decodedURLPage == "")
-//    {
-
-//    }
-//    arrURLPage[5] = numPage;
-//    var newURLPage = arrURLPage.join('/');
-//    var encodedURLPage = encodeURI(newURLPage);
-//    console.log(encodedURLPage);
-//    //return encodedURLPage;
-//}
+var div = document.querySelectorAll('div.node'); // Получаем список все блоков документа
+var cnt = div.length;                            // Считаем количество блок 
+alert(cnt);                                      // Выводи на экран результат
 
 function redirectPage(numPage) {
     var urlPage = window.location.href;
