1c19cdee1099d67582cd5e49a1c8e0562aee4bce
A1604 <anyatropina@mail.ru>
2022-06-17 13:26:27 +0500

    temp pagination

--- NodeManager.Web/wwwroot/js/site.js
+++ NodeManager.Web/wwwroot/js/site.js
@@ -13,6 +13,23 @@ function getValue() {
     //alert(text);
 }
 
+//let page = document.querySelector('.page');
+//let themeButton = document.querySelector('.theme-button');
+//themeButton.onclick = function () {
+//    page.classList.toggle('light-theme');
+//    page.classList.toggle('dark-theme');
+//};
+
+//let message = document.querySelector('.subscription-message');
+//let form = document.querySelector('.subscription');
+//let email = document.querySelector('.subscription-email');
+
+//form.onsubmit = function (evt) {
+//    evt.preventDefault();
+//    // Измените значение textContent на следующей строке
+//    message.textContent = email.value;
+//};
+
 function redirectPage(numPage) {
     var urlPage = window.location.href;
     var decodedURLPage = decodeURI(urlPage);
