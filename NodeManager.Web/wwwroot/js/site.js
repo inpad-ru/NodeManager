9a277ab3eb4f57da3ced03c53968a1713faba178
A1604 <anyatropina@mail.ru>
2022-11-18 12:54:23 +0500

    download file name and picture with proportions

--- NodeManager.Web/wwwroot/js/site.js
+++ NodeManager.Web/wwwroot/js/site.js
@@ -37,3 +37,30 @@ function redirectPage(numPage) {
     var encodedURLPage = encodeURI(newURLPage);
     window.location.assign(encodedURLPage);
 }
+
+var inputs = document.querySelectorAll('.upload-file__input');
+Array.prototype.forEach.call(inputs, function (input) {
+    var label = input.nextElementSibling,
+        labelVal = label.innerHTML;
+    input.addEventListener('change', function (e) {
+        var fileName = '';
+        if (this.files && this.files.length > 1)
+            fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+        else
+            fileName = e.target.value.split('\\').pop();
+
+        if (fileName.length > 18) fileName = fileName.substr(0, 11) + '…' + fileName.substr(name.length - 6);
+        if (fileName.length > 18)
+            fileNameCut = fileName.substr(0, 11) + '…' + fileName.substr(name.length - 6);
+        else fileNameCut = fileName;
+
+        if (fileName)
+            label.querySelector('span').innerHTML = fileName;
+        if (fileNameCut)
+            label.querySelector('span').innerHTML = fileNameCut;
+        else
+            label.innerHTML = labelVal;
+
+        document.getElementsByClassName("upload-file__text")[0].title = fileName;
+    });
+});
